5434be93e21a1726cac937bcce8ccfd0fb931a71
creely001 <creely001@gannon.edu>
2023-01-30 10:16:04 -0500

    added to home page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,26 +8,26 @@ function Cards() {
         <h1>LEARNING PATHS</h1>
         <div className='cards__container'>
             <div className='cards__wrapper'>
-                <ul className='cards__items'>
+                <div className='cards__items'>
                     <CardItem 
-                    src='images/img-1.jpg'
+                    src='images/img-6.png'
                     text='For those who are unfamiliar with technology'
                     label='NOVICE'
                     path='/learning-paths'
                     />
                     <CardItem 
-                    src='images/img-1.jpg'
+                    src='images/img-5.png'
                     text='For those who are familiar with technology'
                     label='APPRENTICE'
                     path='/learning-paths'
                     />
                     <CardItem 
-                    src='images/img-1.jpg'
+                    src='images/img-4.png'
                     text='For those who are well-versed in technology'
                     label='EXPERT'
                     path='/learning-paths'
                     />
-                </ul>
+                </div>
             </div>
         </div>
         <h1>ARTICLES</h1>
@@ -60,19 +60,19 @@ function Cards() {
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
                     <CardItem 
-                    src='images/img-1.jpg'
+                    src='images/img-11.png'
                     text='Can you spot all of the the fishy emails?'
                     label='GO PHISH'
                     path='/games'
                     />
                     <CardItem 
-                    src='images/img-1.jpg'
+                    src='images/img-12.png'
                     text='A crossword with cyber awarenss terms!'
                     label='CROSSWORD'
                     path='/games'
                     />
                     <CardItem 
-                    src='images/img-1.jpg'
+                    src='images/img-13.jpg'
                     text='Can you find all of the hidden words?'
                     label='WORD-SEARCH'
                     path='/games'
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,9 +7,9 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src='/videos/video-2.mp4' autoPlay loop muted />
-        <h1>TEST</h1>
-        <p>subheading test</p>
+        <video src='/videos/video-3.mp4' autoPlay loop muted />
+        <h1>WELCOME TO CYBER GUARDIAN</h1>
+        <p></p>
         <div className='hero-btns'>
           <Button 
             className='btns' 
