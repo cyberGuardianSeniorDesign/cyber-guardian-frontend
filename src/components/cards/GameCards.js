cddd205c3c9951493842da8b206d0a3ca6246c2b
creely001 <creely001@gannon.edu>
2023-02-06 13:34:14 -0500

    pushing final changes before changing repositories

---
+++ src/components/cards/GameCards.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
+
+function GameCards() {
+  return (
+    <div className='cards'>
+        <h1>GAMES</h1>
+        <div className='cards__container'>
+            <div className='cards__wrapper'>
+                <ul className='cards__items'>
+                <CardItem 
+                    src='images/img-11.png'
+                    text='Can you spot all of the the fishy emails?'
+                    label='GO PHISH'
+                    path='/games'
+                    />
+                    <CardItem 
+                    src='images/img-12.png'
+                    text='A crossword with cyber awarenss terms!'
+                    label='CROSSWORD'
+                    path='/games'
+                    />
+                    <CardItem 
+                    src='images/img-13.jpg'
+                    text='Can you find all of the hidden words?'
+                    label='WORD-SEARCH'
+                    path='/games'
+                    />
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default GameCards
