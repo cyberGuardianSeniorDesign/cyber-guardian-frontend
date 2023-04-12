8c052b5fa1b1570393fca337f5d9ba3e03042de4
creely001 <creely001@gannon.edu>
2023-04-12 12:00:33 -0400

    added instructions to wordsearch and changed thumbnail image

--- src/components/pages/WordSearch.jsx
+++ src/components/pages/WordSearch.jsx
@@ -5,10 +5,12 @@ import Menu from '../Games/WordSearch/Menu/Menu.js';
 import Grid from '../Games/WordSearch/Grid/Grid.js';
 import WordList from '../Games/WordSearch/WordList/WordList.js';
 import Footer from '../Footer.jsx';
+import { textAlign } from '@mui/system';
 
 function WordSearch(){
     return( 
         <div>
+            <p style={{fontSize: "1.5rem" , textAlign: "center"}}>Instructions: Search for the hidden cyber awareness terms. Click the first letter of a word and then the last letter to cross them out.</p>
             <Menu />
             <Grid />
             <WordList />
